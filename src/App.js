6f67912956c576c385424b63f44b85467eecef6d
Bruno Paula <brunopaula@dryserv.com>
2020-08-07 17:22:16 -0400

    MVP and Stretch Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { gitHubApi } from "./api/githubApi"
+import "./App.css"
+import User from "./components/user/User"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: [],
+    searchedUser: "devbrunopaula",
+    loading: false,
+  }
+
+  fetchUser = async () => {
+    try {
+      this.setState({ loading: true })
+      const response = await gitHubApi.get(`users/${this.state.searchedUser}`)
+      this.setState({
+        user: response.data,
+        loading: false,
+        searchedUse: response.Login,
+      })
+    } catch (error) {
+      console.log(error)
+      this.setState({ loading: true })
+    }
+  }
+
+  componentDidMount() {
+    this.fetchUser()
+    // setTimeout(() => {
+    //   this.fetchUser()
+    // }, 2000)
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.searchedUser !== prevState.searchedUser) {
+      this.fetchUser()
+    }
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h1 className='app__title'>Github User Card</h1>
+        <form>
+          <div className='user__searchConainter'>
+            <input
+              className='user__searchInput'
+              name='s'
+              type='search'
+              results={5}
+              placeholder='Search a User...'
+              value={this.state.searchedUser}
+              onChange={e => this.setState({ searchedUser: e.target.value })}
+            />
+          </div>
+        </form>
+        <User data={this.state.user} />
+
+        <div className='app__status'>
+          {this.state.searchedUser.length > 1 ? (
+            <img
+              src={`http://ghchart.rshah.org/${this.state.searchedUser}`}
+              alt={`${this.state.searchedUser}'s Github chart`}
+            />
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/user/User.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react"
+import "./user.css"
+import GitHubLogo from "../../assets/imgages/github-logo.png"
+
+const defaultAvatar = "https://www.w3schools.com/howto/img_avatar.png"
+
+class User extends Component {
+  render() {
+    const {
+      avatar_url,
+      name,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos: repo,
+    } = this.props.data
+    return (
+      <>
+        <div className='user'>
+          <div className='user__container'>
+            <div className='user__topAvatar'>
+              <img
+                className='user__avatar'
+                src={avatar_url ? avatar_url : defaultAvatar}
+                alt='avatar'
+              />
+            </div>
+            <div className='user__title--container'>
+              <h3>{name}</h3>
+              <p>@{login}</p>
+              <button
+                className='user__link--button'
+                onClick={() => (window.location.href = html_url)}
+              >
+                <img src={GitHubLogo} alt='github logo' />
+              </button>
+            </div>
+          </div>
+          <div className='user__info'>
+            <div className='user__Followers user__stats'>
+              <h3>Followers</h3>
+              <p>{followers}</p>
+            </div>
+            <div className='user__Following user__stats'>
+              <h3>Following</h3>
+              <p>{following}</p>
+            </div>
+            <div className='user__Repositories user__stats'>
+              <h3>Repositories</h3>
+              <p>{repo}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default User
